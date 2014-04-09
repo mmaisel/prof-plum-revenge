2dc756ccc93c9f9401abd84e4ff4d56cf6d3544b
Matthew Maisel <mmaisel@cylance.com>
2014-04-09 09:32:27 -0400

    fix import of ObjectNode

--- app/controllers/GameAPI.java
+++ app/controllers/GameAPI.java
@@ -3,13 +3,15 @@ package controllers;
 import play.*;
 import play.mvc.*;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.libs.Json;
 
 public class GameAPI extends Controller {
 
     public static Result index(Long uuid) {
         JsonNode json = request().body().asJson();
         if (json == null) {
-            return badReques("No json data, bro.");
+            return badRequest("No json data, bro.");
         }
         ObjectNode gameboard = Json.newObject();
         // lookup game state for a particular game, put into gameboard json struct, send back
