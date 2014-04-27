c7da6c05f32bba75ee5eebc56a0d0825bbe413d7
Matthew Maisel <mmaisel@cylance.com>
2014-04-27 19:37:42 -0400

    reorg to Club/Game layout from Jack, JS still needs to be split up

--- app/controllers/Clueless.java
+++ app/controllers/Clueless.java
@@ -1,39 +1,35 @@
 package controllers;
 
+import java.lang.Math.*;
 import play.*;
-import play.data.*;
 import play.mvc.*;
-import java.util.UUID;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import play.libs.Json;
 
-import views.html.*;
 
 public class Clueless extends Controller {
 
-    public static Result init() {
-        return redirect(controllers.routes.Clueless.index());
+    public static Result index(int club_uuid) {
+        // lookup game state for a particular game, put into gameboard json struct, send back
+        ObjectNode gameboard = Json.newObject();
+        gameboard.put("status", Math.random());
+        return ok(gameboard);
     }
 
-    public static Result index() {
-        String[] openClubUUIDs = {"1", "2", "3"};
-        return ok(index.render("Clueless", openClubUUIDs));
+    public static Result sendMessage(int club_uuid) {
+        JsonNode json = request().body().asJson();
+        if (json == null) {
+            return badRequest("No json data, bro.");
+        }
+        ObjectNode result = Json.newObject();
+        // doAction() on message, get new gamestate, send via gameboard json struct
+        result.put("message", "SUCCESS");
+        result.put("gameboard", "SUCCESS");
+        return ok(result);
     }
 
-    public static Result join() {
-        DynamicForm clubForm = Form.form().bindFromRequest();
-        String player_name = clubForm.get("player_name");
-        int player_uuid = 1;
-        int club_uuid = Integer.parseInt(clubForm.get("club_uuid"));
-
-        return redirect(controllers.routes.Clueless.club(club_uuid, player_name, player_uuid));
-    }
-
-    public static Result plumJs(String player_name, String player_uuid, String club_uuid) {
-        return ok(views.js.plum.render(club_uuid, player_name, player_uuid));
-    }
-
-    public static Result club(int club_uuid, String player_name, int player_uuid) {
-        // session("connected", player_uuid);
-        return ok(club.render("Club Lobby " + club_uuid, player_name, Integer.toString(player_uuid), Integer.toString(club_uuid)));
+    private void doAction() {
     }
 
 }
