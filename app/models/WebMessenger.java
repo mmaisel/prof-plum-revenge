ebd858dbf794f61a128bf1057cd1acc01dc0ef96
jackwilliard <jackfrakn@aol.com>
2014-04-29 19:39:26 -0400

    Example of how to use InfiniteMessenger
    InifiniteMessenger can be used to respond to queries until WebMesenger
    is fully implemented.  Will always give answer back to gameplay
    regardless of server-client interaction.

--- app/models/WebMessenger.java
+++ app/models/WebMessenger.java
@@ -9,6 +9,8 @@ import models.Player.Query;
 
 public class WebMessenger implements IMessenger {
 
+	InfiniteMessenger tempMsgr = new InfiniteMessenger();
+
 	@Override
 	public Object query(Query type, Object...objects) {
 		switch(type) {
@@ -31,7 +33,17 @@ public class WebMessenger implements IMessenger {
                 break;
             }
 		}
-        return null;
+		
+		//There must be a better way to do this...
+		//One day I will learn java, one day.
+        switch(objects.length) {
+		case 2:		
+			return tempMsgr.query(type, objects[0], objects[1]);
+		case 1:		
+			return tempMsgr.query(type, objects[0]);
+		default:		
+			return tempMsgr.query(type);
+		}
 	}
 
 	@Override
